3da8eb368e8519f33cf9e3263b7a124382194068
Jason Yim <jayim243@gmail.com>
2023-06-21 15:40:00 -0700

    Added classes and random button in html, cleaned up css, modofied js accordingly

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // global variable for color of square
 let color = 'black';
-
 // user can click to turn on/off coloring
 let click = true;
+
+
 document.querySelector('body').addEventListener('click', (event) => {
     if (event.target.tagName != 'BUTTON'  && event.target.tagName != 'INPUT') {
         click = !click;
         if (click) {
-            document.querySelector('.mode').textContent = 'Color ON'
+            document.querySelector('.mode').textcontainer = 'Color ON'
             document.querySelector('.mode').style.color = 'rgb(198, 226, 43)'
         } else {
-            document.querySelector('.mode').textContent = 'Color OFF'
+            document.querySelector('.mode').textcontainer = 'Color OFF'
             document.querySelector('.mode').style.color = 'rgb(226, 43, 67)'
         }
     }
 })
+
 
 // function that populates the grid
 function createBoard(num) {
@@ -32,39 +34,47 @@ function createBoard(num) {
         // event listener to change color when mouse is over squares
         square.addEventListener('mouseover', changeColor);
         square.style.backgroundColor = 'white';
-        grid.appendChild(square)
+        grid.appendChild(square);
         // grid.insertAdjacentElement('beforeend', square);
     }
 } 
 
+
+// default beginning board
 createBoard(16)
+
+
 // function that changes size of grid given valid user inputs
 function changeSize(userInput) {
     if (userInput >= 2 && userInput <= 100) {
-        document.querySelector('.error').style.display = 'none'
+        document.querySelector('.error').textContent = "";
         createBoard(userInput);
     } else {
-        document.querySelector('.error').style.display = 'flex'
+        document.querySelector('.error').textContent = "Size must be between 2 and 100";
+        document.querySelector('.error').style.color = 'yellow';
+        document.querySelector('.error').style.fontSize = '30px';
     }
 }
+
 
 // this function is actually being created num times: each changeColor function is focusing only on a singular div
 function changeColor() {
     if (click) {
-        this.style.backgroundColor=color;
+        if (color === 'random') {
+            // randomized color formula
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor=color;
+        }
     }
-    // if (color == 'random') {
-    //     this.style.backgroundColor = hs1($(Math.random() * 360));
-    // } else {
-        
-    // }
-    
 }
+
 
 // function to change color of square when buttons are pressed
 function buttonColor(button) {
     color = button;
 }
+
 
 // function for reset button(basically resets board)
 function buttonReset() {
